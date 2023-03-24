9242e70bddd8448bdbb507ea58a2002475f3f0ca
phillipnzau <nzauonline78@gmail.com>
2023-03-24 12:38:14 +0300

    flex items on the nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        "navHeight": "5rem"
+
+      }
+    },
   },
   plugins: [],
 }
