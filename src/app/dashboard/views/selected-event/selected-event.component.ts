fb495de083831b043aa035e43a14dee0a638887e
phillipnzau <nzauonline78@gmail.com>
2023-04-04 11:39:49 +0300

    card update

--- src/app/dashboard/views/selected-event/selected-event.component.ts
+++ src/app/dashboard/views/selected-event/selected-event.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FcTableComponent } from "../../components/fc-table/fc-table.component";
+import { ActivatedRoute } from '@angular/router';
+import { FcCardComponent } from "../../components/fc-card/fc-card.component";
 
 @Component({
-  selector: 'app-selected-event',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './selected-event.component.html',
-  styleUrls: ['./selected-event.component.scss']
+    selector: 'app-selected-event',
+    standalone: true,
+    templateUrl: './selected-event.component.html',
+    styleUrls: ['./selected-event.component.scss'],
+    imports: [CommonModule, FcTableComponent, FcCardComponent]
 })
-export class SelectedEventComponent {
+export class SelectedEventComponent implements OnInit {
+  organisations = [1,2,3,4];
+  route: any
 
+  constructor(private activeRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+
+    
+  }
 }
