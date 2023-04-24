827be8da7c7e694fefeb86d2311fda57a5158fee
phillipnzau <nzauonline78@gmail.com>
2023-04-24 14:43:12 +0300

    Env file

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,9 +2,9 @@ const url = 'http://176.58.115.160:3000/api/v1/';
 const loginUrl = url + 'login/';
 const accountsBaseUrl = url + 'accounts/';
 const organizationBaseUrl = url + 'organizations/';
-const projectBaseUrl = url + 'project/';
-const attendeeBaseUrl = url + 'attendee/';
-const eventBaseUrl = url + 'event/';
+const projectBaseUrl = url + 'projects/';
+const attendeeBaseUrl = url + 'attendees/';
+const eventBaseUrl = url + 'events/';
 
 export const environment = {
     ///////////////** USER URLS **///////////////////
@@ -22,10 +22,41 @@ export const environment = {
     updateOrganization: organizationBaseUrl,
     getOneOrganization: organizationBaseUrl,
 
-    createOrganizationMember: organizationBaseUrl + 'member/',
-    getAllOrganizationMember: organizationBaseUrl + 'member/',
+    createOrganizationMember: organizationBaseUrl + 'member',
+    getAllOrganizationMember: organizationBaseUrl + 'member',
     deleteOrganizationMember: organizationBaseUrl + 'member/',
     getOneOrganizationMember: organizationBaseUrl + 'member/',
+
+    ///////////////** Projects URLS **///////////////////
+    getAllProjects: projectBaseUrl,
+    createProjects: projectBaseUrl,
+    updateProject: projectBaseUrl,
+    getSingleProjects: projectBaseUrl,
+
+    ///////////////** Attendee URLS **///////////////////
+    getAllAttendees: attendeeBaseUrl,
+    createAttendees: attendeeBaseUrl,
+    getSingleAttendees: attendeeBaseUrl,
+    updateAttendees: attendeeBaseUrl,
+
+    ///////////////** Events URLS **///////////////////
+    getAllEvents: eventBaseUrl,
+    createEvents: eventBaseUrl,
+    getSingleEvents: eventBaseUrl,
+
+    getAllEventAttendees: eventBaseUrl + 'attendees/',
+    createEventAttendees: eventBaseUrl + 'attendees/',
+    getSingleEventAttendees: eventBaseUrl + 'attendees/',
+
+    getAllEventAttendeesCheckIn: eventBaseUrl + 'attendees/check-in',
+    createEventAttendeesCheckIn: eventBaseUrl + 'attendees/check-in',
+    getSingleEventAttendeesCheckIn: eventBaseUrl + 'attendees/check-in/',
+    updateEventAttendeesCheckIn: eventBaseUrl + 'attendees/check-in/',
+
+
+
+
+    
 
 
     
